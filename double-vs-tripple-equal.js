4311c73740d8f8e1b34518b6eeb3a225d176e6ea
unknown <somrathumayan@gmail.com>
2024-03-26 23:08:04 +0600

    javascript advanced - double vs tripple equal

---
+++ double-vs-tripple-equal.js
@@ -0,0 +1,13 @@
+const first = 5;
+const second = "5";
+
+if(first === second){
+    console.log("Condition is true....");
+}
+else{
+    console.log("Condition is false....");
+}
+
+// == equal hole type check korbena ek rokom holei true dekhabe.. 
+
+// triple === equal hole type theke suru kore sob kisu compare kore true or false dekhabe 
