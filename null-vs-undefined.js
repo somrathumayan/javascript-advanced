592e9596693e2e586baf2fe8238683c6c677b12a
unknown <somrathumayan@gmail.com>
2024-03-26 22:59:14 +0600

    javascript advanced - null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,15 @@
+let name;
+console.log(name); //output : undefined
+
+
+function add(num1, num2){
+    console.log(num1+num2);
+    // return num1+num2;
+}
+const result = add(15, 15);
+console.log(result); //return na thakle output : undefinedm dekhabe..
+
+// parameter pass na hole undefined dekhabe
+
+const employee = {name:"Md. Humayan Kabir", phone:01794556952};
+console.log(employee.salary);
